1e7947bcb1bc3ea94a92b8f052dca6083231005c
Eric DeSimone <eric.desimone@cortland.edu>
2018-06-29 13:05:37 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     time = Time.now
-    if time >= 12:00
+    if time >= 1200
       resp.write "Good Afternoon"
     else
       resp.write "Good Morning"
